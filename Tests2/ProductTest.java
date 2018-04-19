9e5a9231661244e475e03c05642c7ab26180b629
Farhad Ul Islam <farhad.islam@outlook.com>
2018-04-19 15:02:57 +0100

    Added Product and a seperate test folder for TEST

---
+++ Tests2/ProductTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductTest {
+
+    @Test
+    public void getProductId() {
+    }
+
+    @Test
+    public void setProductId() {
+    }
+
+    @Test
+    public void getProductName() {
+    }
+
+    @Test
+    public void setProductName() {
+    }
+}
