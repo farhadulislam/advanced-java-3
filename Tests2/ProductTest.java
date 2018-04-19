a78e1c62dbbb83016560410e95020ee287b7b9a3
Farhad Ul Islam <farhad.islam@outlook.com>
2018-04-19 15:15:58 +0100

    Product class Test. ALL TESTS PASSED

--- Tests2/ProductTest.java
+++ Tests2/ProductTest.java
@@ -1,22 +1,54 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ProductTest {
+    private Product p1;
+    private Product p2;
+
+
+    @Before
+    public void setUp() throws Exception {
+
+        p1 = new Product(1, "Table");
+        p2 = new Product(1, "Chair");
+
+
+
+    }
 
     @Test
     public void getProductId() {
+
+        assertEquals(1, p1.getProductId());
     }
 
     @Test
     public void setProductId() {
+
+
+        p1.setProductId(-233);
+        assertEquals(1, p1.getProductId());
+
     }
 
     @Test
     public void getProductName() {
+
+        assertEquals("Table", p1.getProductName());
     }
 
     @Test
     public void setProductName() {
+
+        p1.setProductName("Banana");
+        assertNotEquals("Banana", p1.getProductName());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
     }
 }
--- src/Product.java
+++ src/Product.java
@@ -1,27 +1,31 @@
 public class Product {
 
     private int productId;
-    private String productName;
+    private final String productName;
+    private static int numberOfProducts = 0;
 
     public Product(int productId, String productName){
 
         this.productId = productId;
         this.productName = productName;
+        numberOfProducts++;
     }
 
-  /*  public int getProductId() {
+ public int getProductId() {
         return productId;
     }
 
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
+   public void setProductId(int productId) {
+        if(productId > 0) {
+            this.productId = productId;
+        }
+   }
 
-    public String getProductName() {
-        return productName;
-    }
+   public String getProductName() {
+       return productName;
+   }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }*/
+   public void setProductName(String productName) {
+       //this.productName = productName;
+    }
 }
