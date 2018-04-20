ffce448a13ab234d7bcda5196c1f1848223b1992
Farhad Ul Islam <farhad.islam@outlook.com>
2018-04-20 14:51:42 +0100

    Injector class refactored

--- src/genericDemo/Injector.java
+++ src/genericDemo/Injector.java
@@ -1,35 +1,47 @@
 package genericDemo;
 
+
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
-public class Injector {
-
+public class Injector
+{
     private Map<Class<?>, Object> objectGraph = new HashMap<>();
 
-    public Injector with(Object value){
+    public <T> T newInstance(Class<T> type)
+    {
+        return (T) objectGraph.computeIfAbsent(type, this::instantiate);
+    }
+
+    public Injector with(Object value)
+    {
         objectGraph.put(value.getClass(), value);
         return this;
     }
-    public <T> T newInstance(final Class<T> type){
 
-
-        return (T) objectGraph.computeIfAbsent(type, this ::instantiate);
-    }
-
-    public Object instantiate(Class<?> type){
-
-        try {
+    private Object instantiate(Class<?> type)
+    {
+        try
+        {
             Constructor<?>[] constructors = type.getConstructors();
-            if (constructors.length !=1){
-
+            if (constructors.length != 1)
+            {
+                throw new IllegalArgumentException(type + " must only have 1 constructor");
             }
-        } catch (Exception e) {
 
+            Constructor<?> constructor = constructors[0];
+            Object[] args = Stream.of(constructor.getParameterTypes())
+                    .map(param -> (Object) newInstance(param))
+                    .toArray();
+
+            return constructor.newInstance(args);
         }
-
-
+        catch (InstantiationException | IllegalAccessException | InvocationTargetException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
-    //public Object newInstance(final Class<?> className){return null; }
 }
