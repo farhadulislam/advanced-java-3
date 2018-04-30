4131b5210b67047486da2fd46a310b6b6c597c46
Farhad Ul Islam <farhad.islam@outlook.com>
2018-04-30 17:01:03 +0100

    Just added another lambda exp with single param

--- src/lambdaDemonstration1/LambdaDemo1.java
+++ src/lambdaDemonstration1/LambdaDemo1.java
@@ -11,8 +11,11 @@ public class LambdaDemo1 {
         System.out.println(myNum.getValue());
 
         NumericTest isEven;
+        NumericTest isEven2;
 
         isEven = (n)-> n%2==0;
-        if(isEven.isEvenTest(23)) System.out.println("The number is even");
+        isEven2 = n-> n%2==0; // for single param, it is fine to take parenthesis off.
+        if(isEven.isEvenTest(22)) System.out.println("The number is even");
+        if(isEven2.isEvenTest(50)) System.out.println("The number is even");
     }
 }
