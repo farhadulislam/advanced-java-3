8f8114f8c84fb5f4e65aecb1f1518e3039fd3c33
Farhad Ul Islam <farhad.islam@outlook.com>
2018-05-16 16:15:29 +0100

    HashMapDemo1 added

---
+++ src/genericDemonstration1/HashMapDemo1.java
@@ -0,0 +1,15 @@
+package genericDemonstration1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo1 {
+
+    public final static HashMap<Integer, String> map1 = new HashMap<Integer, String>(){
+       // put(23,"Three");
+    };
+
+    public static void main (String ... args) {
+
+    }
+}
