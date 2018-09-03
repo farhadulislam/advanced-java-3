03fd62d3318d965b05185840e6cf8ba97a867a2e
Farhad Ul Islam <farhad.islam@outlook.com>
2018-09-03 17:29:58 +0100

    added a dummy class

---
+++ src/genericDemonstration1/Dummy.java
@@ -0,0 +1,15 @@
+package genericDemonstration1;
+
+public class Dummy {
+
+    public static void main (String [] args){
+
+        doSomething();
+    }
+
+    public static void doSomething(){
+
+        System.out.println("doing something .....");
+
+    }
+}
