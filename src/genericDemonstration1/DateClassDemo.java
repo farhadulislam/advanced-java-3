498aba793f3a6c2ec58cf3d46c74ea4ecf6efb4e
Farhad Ul Islam <farhad.islam@outlook.com>
2018-10-15 14:23:53 +0100

    using datetime methods and time in millisecond

--- src/genericDemonstration1/DateClassDemo.java
+++ src/genericDemonstration1/DateClassDemo.java
@@ -1,4 +1,30 @@
 package genericDemonstration1;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateClassDemo {
+
+    static{
+
+    }
+
+    public static void main (String [] args){
+
+        System.out.println("Date class Demo");
+
+        Date date = new Date();
+
+        System.out.println(date);
+
+        long timeInMilisec = date.getTime();
+
+        System.out.println(timeInMilisec);
+        int hours   = (int) ((timeInMilisec / (1000*60*60)) % 24);
+
+        System.out.println(hours);
+
+
+
+    }
 }
