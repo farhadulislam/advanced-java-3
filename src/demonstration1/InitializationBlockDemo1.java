d30a07910e54f77eaea498641b1252048d6b1449
Farhad Ul Islam <farhad.islam@outlook.com>
2018-10-24 10:39:25 +0100

    added a new package demonstration1 for all kinds of demo and practices. added a class with initialization block in

---
+++ src/demonstration1/InitializationBlockDemo1.java
@@ -0,0 +1,19 @@
+package demonstration1;
+
+public class InitializationBlockDemo1 {
+    static {
+        int capacity = 20;
+    }
+
+    public static int capacity2 = initializeClassVariable();
+
+    public static void main (String [] args){
+
+        System.out.println("There is static block");
+    }
+
+    private static int initializeClassVariable(){
+
+        return 25;
+    }
+}
