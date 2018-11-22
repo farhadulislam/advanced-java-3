69b87206d9f214bce1431e81a3fcdc47aae00911
Farhad Ul Islam <farhad.islam@outlook.com>
2018-11-22 17:06:36 +0000

    added TypeDemo1 class

---
+++ src/demonstration2/TypeDemo1.java
@@ -0,0 +1,22 @@
+package demonstration2;
+
+public class TypeDemo1 {
+
+    public static void main (String [] args){
+
+        char charVal = 'a';
+
+
+        float floatVal = .8f;
+        double doubleVal = 23.23d;
+
+
+        byte byteVal = 8;
+        short shortVal = 23;
+        long longVal = 34;
+
+        int [] intArray = {0,1, 2, 3, 4};
+        char [] charArray = {'a', 'b', 'f'};
+
+    }
+}
