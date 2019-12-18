1cac8707d7f97aa888f8c4b38f6af789a8522ed5
Farhad Ul Islam <farhad.islam@outlook.com>
2019-12-18 17:30:39 +0000

    added constructor ref demo

---
+++ src/lambdaDemonstration1/ConstructorRefDemo.java
@@ -0,0 +1,18 @@
+package lambdaDemonstration1;
+interface MyFunc5{
+
+    MyClass5 int myFunc(int v);
+
+}
+
+class MyClass5{
+    
+
+}
+public class ConstructorRefDemo {
+
+    public static void main (String [] args){
+
+
+    }
+}
