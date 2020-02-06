878c0124ce11d9de4ecdb9ad4ef001a1db968fa5
Farhad Ul Islam <farhad.islam@outlook.com>
2020-02-06 17:27:56 +0000

    updated thread demo - implemented a main thread withing main method

--- src/multithreadingDemo1/ThreadDemo.java
+++ src/multithreadingDemo1/ThreadDemo.java
@@ -36,5 +36,23 @@ public class ThreadDemo {
     public static void main (String [] args){
 
 
+        Thread nt = new Thread();
+
+        nt.start();
+
+        try{
+            for(int i =5 ; i >0 ; i--){
+
+                System.out.println("Main thread: "+ i);
+
+                Thread.sleep(1000);
+            }
+
+        }catch(InterruptedException e){
+            System.out.println("Main thread interrupted");
+        }
+
+
+
     }
 }
