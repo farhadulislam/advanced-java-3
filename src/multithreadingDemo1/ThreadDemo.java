7788656ebed44c9a9d3dbf2f5f42b1d969ee88b6
Farhad Ul Islam <farhad.islam@outlook.com>
2020-02-06 17:09:41 +0000

    added a thread demo class that implements runnable interface

---
+++ src/multithreadingDemo1/ThreadDemo.java
@@ -0,0 +1,40 @@
+package multithreadingDemo1;
+
+
+class NewThread implements Runnable{
+
+    Thread t;
+
+    NewThread(){
+
+        t = new Thread(this, "demo thread");
+    }
+
+
+    @Override
+    public void run() {
+
+        try{
+
+            for (int i =5; i>0; i--){
+
+                System.out.println("Child thread: "+ i);
+
+                Thread.sleep(1000);
+
+            }
+        }catch (InterruptedException e){
+
+            System.out.println("Child thread interrupted");
+
+        }
+
+    }
+}
+public class ThreadDemo {
+
+    public static void main (String [] args){
+
+
+    }
+}
