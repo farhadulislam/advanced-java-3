66433114460d70703f21afe562390edb0257edd4
Farhad Ul Islam <farhad.islam@outlook.com>
2020-02-11 16:50:37 +0000

    added a utility class within multithreading demo package

---
+++ src/multithreadingDemo1/UtiliyClass.java
@@ -0,0 +1,18 @@
+package multithreadingDemo1;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+public class UtiliyClass {
+    // This class intended to be used for practices or any idea that comes up as I practise multithreading
+    // It may not be related to multithreading.
+    public static void main (String [] args){
+
+        System.out.println(getBooleanInstance(true));
+    }
+
+    public static Boolean getBooleanInstance (boolean val ){
+
+        return val ? TRUE: FALSE;
+    }
+}
