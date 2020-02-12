2238d84912e7cd7e22403fc179e48cd87bce3c45
Farhad Ul Islam <farhad.islam@outlook.com>
2020-02-12 17:18:04 +0000

    added join demo (uses join() and isAlive() methods)

---
+++ src/multithreadingDemo1/DemoJoin.java
@@ -0,0 +1,47 @@
+package multithreadingDemo1;
+
+class NewThread4 implements Runnable {
+
+    String name;
+    Thread t;
+
+    NewThread4(String threadName){
+
+        name = threadName;
+        t = new Thread(this, name);
+
+        System.out.println ("New Thread "+ t);
+    }
+    public void run()
+    {
+        try {
+
+            for (int i = 5; i > 0; i--) {
+                // Displaying the thread that is running
+                System.out.println(name + " : " + i);
+                Thread.sleep(1000);
+
+            }
+        }
+        catch (InterruptedException e) {
+            // Throwing an exception
+            System.out.println (name + " interrupted");
+        }
+        System.out.println (name + "  exiting");
+    }
+}
+
+
+public class DemoJoin {
+
+    public static void main (String [] args){
+
+        NewThread4 nt1 = new NewThread4("One");
+        NewThread4 nt2 = new NewThread4("Two");
+        NewThread4 nt3 = new NewThread4("Three");
+
+        nt1.t.start();
+        nt2.t.start();
+        nt3.t.start();
+    }
+}
