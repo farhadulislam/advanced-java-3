9eab090a3a17b11f820b6dbe93b764e8ef83de2f
Farhad Ul Islam <farhad.islam@outlook.com>
2020-02-26 14:58:23 +0000

    Partially fixed PC  implementation

--- src/multithreadingDemo1/PCFixed.java
+++ src/multithreadingDemo1/PCFixed.java
@@ -1,15 +1,36 @@
 package multithreadingDemo1;
 class Q{
 int n;
+boolean valueSet = false;
 
 synchronized int get(){
+    while(!valueSet){
+        try{
+            wait();
+        }catch (InterruptedException e){
+            System.out.println("InterruptedException caught");
+        }
+        }
+
     System.out.println("Got: "+ n);
+    valueSet=false;
+    notify();
     return n;
 }
 
 synchronized void put(int n){
-    this.n = n;
-    System.out.println("Put: "+ n);
+    while(valueSet){
+        try{
+            wait();
+        }catch (InterruptedException e){
+            System.out.println("InterruptedException caught");
+        }
+    }
+
+   this.n = n;
+    valueSet=true;
+    notify();
+    System.out.println("Got: "+ n);
 
     }
 
@@ -47,13 +68,13 @@ class Consumer implements Runnable{
         }
     }
 }
-public class PC {
+public class PCFixed {
     public static void main(String[] args) {
         Q q = new Q();
         Producer p = new Producer();
         Consumer c = new Consumer();
 
-        
+
         //start the threads
         p.t.start();
         c.t.start();
